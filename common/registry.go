0d940e40b5c288188a63ea5519dd06d3f3939e4c
Andre Bartke <andre@bartke.cc>
2017-11-25 23:48:52 +0800

    split out registry into package

---
+++ common/registry.go
@@ -0,0 +1,154 @@
+package common
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+type Registry struct {
+	ch   *amqp.Channel
+	pool sync.Map
+
+	addConsumer    chan string
+	deleteConsumer chan string
+	consumerCount  int
+}
+
+func NewRegistry(ch *amqp.Channel, timeout time.Duration) (*Registry, error) {
+	q := "register"
+	_, err := SetupQueue(ch, q)
+	if err != nil {
+		return nil, err
+	}
+
+	err = BindRegistry(ch, q)
+	if err != nil {
+		return nil, err
+	}
+
+	msgs, err := Consume(ch, q, "mgnt")
+	if err != nil {
+		return nil, err
+	}
+
+	r := &Registry{
+		ch:             ch,
+		addConsumer:    make(chan string, 2),
+		deleteConsumer: make(chan string, 2),
+	}
+	go r.runRegistry(msgs)
+	go r.runCheckout(timeout)
+	go r.runBalancer()
+	return r, nil
+}
+
+func (r *Registry) ConsumerCount() int {
+	return r.consumerCount
+}
+
+func (r *Registry) runBalancer() {
+	for {
+		select {
+		case k := <-r.addConsumer:
+			r.consumerCount++
+			fmt.Println("adding consumer to pool:", k)
+		case k := <-r.deleteConsumer:
+			r.consumerCount--
+			fmt.Println("removing consumer from pool:", k)
+		}
+		err := r.balanceBindings(r.consumerCount)
+		if err != nil {
+			fmt.Println("Failed to balance bindings", err)
+		}
+	}
+}
+
+func (r *Registry) runCheckout(timeout time.Duration) {
+	for {
+		time.Sleep(timeout / 2)
+		r.pool.Range(func(ki, vi interface{}) bool {
+			k := ki.(string)
+			v := vi.(int64)
+			lastCheckin := time.Since(time.Unix(0, v))
+			if lastCheckin > timeout {
+				r.pool.Delete(k)
+				r.deleteConsumer <- k
+			}
+			return true
+		})
+	}
+}
+
+func (r *Registry) runRegistry(msgs <-chan amqp.Delivery) {
+	for d := range msgs {
+		t := time.Now().UnixNano()
+		k := string(d.Body)
+
+		// check in and refresh
+		if _, ok := r.pool.LoadOrStore(k, t); !ok {
+			r.addConsumer <- k
+			continue
+		}
+		r.pool.Store(k, t)
+	}
+}
+
+func (r *Registry) balanceBindings(n int) error {
+	if n == 0 {
+		return fmt.Errorf("no consumers")
+	}
+	fmt.Println("balancing for", n, "consumers")
+	alphabet := len(RouteKeys)
+	chunks := int(math.Ceil(float64(alphabet) / float64(n)))
+	chunki := 0
+
+	type binding struct {
+		k, route string
+	}
+	var bindings []binding
+	var unbindings []binding
+
+	r.pool.Range(func(ki, v interface{}) bool {
+		k := ki.(string)
+		s := chunki * chunks
+		e := (chunki + 1) * chunks
+		for i := 0; i < alphabet; i++ {
+			route := string(RouteKeys[i])
+			if i >= s && i < e {
+				bindings = append(bindings, binding{k: k, route: route})
+			} else {
+				unbindings = append(unbindings, binding{k: k, route: route})
+			}
+		}
+		chunki++
+		return true
+	})
+
+	for i := range bindings {
+		b := bindings[i]
+
+		// bind route to k
+		fmt.Println("binding", b.k, "to", b.route)
+		err := BindQueueTopic(r.ch, b.k, b.route)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	for i := range unbindings {
+		b := unbindings[i]
+
+		// unbind route from all but k
+		fmt.Println("unbinding", b.k, "from", b.route)
+		err := UnbindQueueTopic(r.ch, b.k, b.route)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	return nil
+}
--- producer/main.go
+++ producer/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/bartke/go-rabbitmq-partitioned-jobs/common"
@@ -28,7 +26,8 @@ func main() {
 	err = common.SetupDatafeedExchange(ch)
 	failOnError(err, "Failed to declare a exchange")
 
-	registry := NewRegistry(ch)
+	registry, err := common.NewRegistry(ch, consumerTimeout)
+	failOnError(err, "Failed to create registry")
 
 	// forever
 	var counter int
@@ -54,142 +53,4 @@ func failOnError(err error, msg string) {
 		log.Fatalf("%s: %s", msg, err)
 		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
-}
-
-type Registry struct {
-	ch   *amqp.Channel
-	pool sync.Map
-
-	addConsumer    chan string
-	deleteConsumer chan string
-	consumerCount  int
-}
-
-func NewRegistry(ch *amqp.Channel) *Registry {
-	q := "register"
-	_, err := common.SetupQueue(ch, q)
-	failOnError(err, "Failed to declare queue")
-
-	err = common.BindRegistry(ch, q)
-	failOnError(err, "Failed to bind queue")
-
-	msgs, err := common.Consume(ch, q, "mgnt")
-	failOnError(err, "Failed to register a consumer")
-
-	r := &Registry{
-		ch:             ch,
-		addConsumer:    make(chan string, 2),
-		deleteConsumer: make(chan string, 2),
-	}
-	go r.runRegistry(msgs)
-	go r.runCheckout()
-	go r.runBalancer()
-	return r
-}
-
-func (r *Registry) ConsumerCount() int {
-	return r.consumerCount
-}
-
-func (r *Registry) runBalancer() {
-	for {
-		select {
-		case k := <-r.addConsumer:
-			r.consumerCount++
-			fmt.Println("adding consumer to pool:", k)
-		case k := <-r.deleteConsumer:
-			r.consumerCount--
-			fmt.Println("removing consumer from pool:", k)
-		}
-		err := r.balanceBindings(r.consumerCount)
-		if err != nil {
-			fmt.Println("Failed to balance bindings", err)
-		}
-	}
-}
-
-func (r *Registry) runCheckout() {
-	for {
-		time.Sleep(consumerTimeout / 2)
-		r.pool.Range(func(ki, vi interface{}) bool {
-			k := ki.(string)
-			v := vi.(int64)
-			lastCheckin := time.Since(time.Unix(0, v))
-			if lastCheckin > consumerTimeout {
-				r.pool.Delete(k)
-				r.deleteConsumer <- k
-			}
-			return true
-		})
-	}
-}
-
-func (r *Registry) runRegistry(msgs <-chan amqp.Delivery) {
-	for d := range msgs {
-		t := time.Now().UnixNano()
-		k := string(d.Body)
-
-		// check in and refresh
-		if _, ok := r.pool.LoadOrStore(k, t); !ok {
-			r.addConsumer <- k
-			continue
-		}
-		r.pool.Store(k, t)
-	}
-}
-
-func (r *Registry) balanceBindings(n int) error {
-	if n == 0 {
-		return fmt.Errorf("no consumers")
-	}
-	fmt.Println("balancing for", n, "consumers")
-	alphabet := len(common.RouteKeys)
-	chunks := int(math.Ceil(float64(alphabet) / float64(n)))
-	chunki := 0
-
-	type binding struct {
-		k, route string
-	}
-	var bindings []binding
-	var unbindings []binding
-
-	r.pool.Range(func(ki, v interface{}) bool {
-		k := ki.(string)
-		s := chunki * chunks
-		e := (chunki + 1) * chunks
-		for i := 0; i < alphabet; i++ {
-			route := string(common.RouteKeys[i])
-			if i >= s && i < e {
-				bindings = append(bindings, binding{k: k, route: route})
-			} else {
-				unbindings = append(unbindings, binding{k: k, route: route})
-			}
-		}
-		chunki++
-		return true
-	})
-
-	for i := range bindings {
-		b := bindings[i]
-
-		// bind route to k
-		fmt.Println("binding", b.k, "to", b.route)
-		err := common.BindQueueTopic(r.ch, b.k, b.route)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	for i := range unbindings {
-		b := unbindings[i]
-
-		// unbind route from all but k
-		fmt.Println("unbinding", b.k, "from", b.route)
-		err := common.UnbindQueueTopic(r.ch, b.k, b.route)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	return nil
 }
