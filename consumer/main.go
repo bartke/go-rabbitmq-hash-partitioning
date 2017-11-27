4df30688493a178f20fe2135bc76a59796980e01
Andre Bartke <andre@bartke.cc>
2017-11-27 11:54:48 +0800

    gracefully shutdown consumer

--- consumer/main.go
+++ consumer/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/bartke/go-rabbitmq-partitioned-jobs/common"
@@ -27,6 +30,8 @@ func init() {
 
 func main() {
 	flag.Parse()
+	exitChan := make(chan struct{})
+	var wg sync.WaitGroup
 
 	conn, err := amqp.Dial(common.ConnectionString())
 	failOnError(err, "Failed to connect to RabbitMQ")
@@ -48,22 +53,39 @@ func main() {
 	msgs, err := common.Consume(ch, tag, "comsumer-"+tag)
 	failOnError(err, "Failed to register a consumer")
 
-	forever := make(chan bool)
-
 	// register and keep alive
 	// receive
-	go func() {
-		for d := range msgs {
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		select {
+		case d := <-msgs:
 			fmt.Printf(" [<-] %s received: %s via %s\n", tag, d.Body, d.RoutingKey)
 			d.Ack(false)
+		case <-exitChan:
+			defer func(wg *sync.WaitGroup) {
+				wg.Done()
+			}(wg)
+			fmt.Println("shutting down consumer")
+			return
 		}
-	}()
+	}(&wg)
 
 	// start pinging after we're listening
-	go heartbeat(ch, tag)
+
+	wg.Add(1)
+	go heartbeat(ch, tag, &wg, exitChan)
 
 	fmt.Printf(" [**] %s running\n", tag)
-	<-forever
+	// blocking wait for SIGTERM
+	waitForSigterm()
+
+	fmt.Printf(" [**] %s trying to shutdown..\n", tag)
+	// after SIGTERM trigger exit and wait for all procedures to finish
+	close(exitChan)
+	wg.Wait()
+	// detach
+	ch.Close()
+	fmt.Printf(" [**] %s graceful shutdown\n", tag)
 }
 
 func failOnError(err error, msg string) {
@@ -73,10 +95,24 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func heartbeat(ch *amqp.Channel, consumer string) {
+func heartbeat(ch *amqp.Channel, consumer string, wg *sync.WaitGroup, exitChan chan struct{}) {
+	t := time.NewTicker(heartbeatFrequency)
 	for {
-		err := common.Heartbeat(ch, consumer)
-		failOnError(err, "Failed to publish a message")
-		time.Sleep(heartbeatFrequency)
+		select {
+		case <-t.C:
+			err := common.Heartbeat(ch, consumer)
+			failOnError(err, "Failed to publish a message")
+		case <-exitChan:
+			fmt.Println("shutting down heartbeats")
+			wg.Done()
+			return
+		}
 	}
+}
+
+func waitForSigterm() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGINT)
+	<-c
 }
